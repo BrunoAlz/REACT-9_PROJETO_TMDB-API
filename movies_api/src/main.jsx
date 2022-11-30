ab1efcbc22c0774d256b9d72b0f16ba6dbc241ae
Bruno Alves <bruno6821@gmail.com>
2022-11-30 03:19:12 -0300

    Limpeza inicial do Projeto, configuração de ROTAS

--- movies_api/src/main.jsx
+++ movies_api/src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/movie/:id" element={<Movie />}></Route>
+          <Route path="search" element={<Search />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
-)
+);

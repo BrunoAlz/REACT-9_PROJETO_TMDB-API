95bc4627d76c4d7b01a7e4f5466b2ff5f6f49ee8
Bruno Alves <bruno6821@gmail.com>
2022-11-30 03:45:15 -0300

    Componentizando o Navbar

--- movies_api/src/App.jsx
+++ movies_api/src/App.jsx
@@ -1,21 +1,18 @@
 import "./App.css";
 
 // Router
-import { Link, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 // Hooks
 import { useState } from "react";
 
+// Components
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <div className="App">
-      <nav id="navbar">
-        <h2>
-          <Link to="/">TMDB-API</Link>
-        </h2>
-        <Link to="/movie/1">TMDB-API</Link>
-        <Link to="/search">TMDB-API</Link>
-      </nav>
+      <Navbar />
       <h2>Movies API</h2>
       <Outlet />
     </div>
---
+++ movies_api/src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+// Routes
+import { Link } from "react-router-dom";
+
+// Icons
+import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
+
+const Navbar = () => {
+  return (
+    <nav id="navbar">
+      <h2>
+        <Link to="/">
+          <BiCameraMovie /> TMDB-API
+        </Link>
+      </h2>
+      <form>
+        <input type="text" placeholder="Buscar filme.." />
+        <button type="submit">
+          <BiSearchAlt2 />
+        </button>
+      </form>
+    </nav>
+  );
+};
+
+export default Navbar;

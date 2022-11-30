c0ea906340819fd34b89ed9a2914216908db45e5
Bruno Alves <bruno6821@gmail.com>
2022-11-30 03:34:50 -0300

    Implementando a Navbar e os Links Iniciais

--- movies_api/src/App.jsx
+++ movies_api/src/App.jsx
@@ -1,15 +1,25 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
 
+// Router
+import { Link, Outlet } from "react-router-dom";
 
+// Hooks
+import { useState } from "react";
 
 function App() {
-
   return (
     <div className="App">
-      <h2>TMDB - API</h2>
+      <nav id="navbar">
+        <h2>
+          <Link to="/">TMDB-API</Link>
+        </h2>
+        <Link to="/movie/1">TMDB-API</Link>
+        <Link to="/search">TMDB-API</Link>
+      </nav>
+      <h2>Movies API</h2>
+      <Outlet />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;

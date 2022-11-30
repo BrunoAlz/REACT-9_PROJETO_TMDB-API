f75791900e5b59f109f610ed9815c13d81c46842
Bruno Alves <bruno6821@gmail.com>
2022-11-30 03:28:18 -0300

    Criação inicial das Paginas da API

--- movies_api/src/main.jsx
+++ movies_api/src/main.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+
+// CSS
 import "./index.css";
+
+// Pages
+import App from "./App";
+import Home from "./Pages/Home/Home";
+import Movie from "./Pages/Movie/Movie";
+import Search from "./Pages/Search/Search";
+
+// Router
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -9,9 +18,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <BrowserRouter>
       <Routes>
         <Route element={<App />}>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/movie/:id" element={<Movie />}></Route>
-          <Route path="search" element={<Search />}></Route>
+          <Route path="/" element={<Home />} />
+          <Route path="/movie/:id" element={<Movie />} />
+          <Route path="search" element={<Search />} />
         </Route>
       </Routes>
     </BrowserRouter>
